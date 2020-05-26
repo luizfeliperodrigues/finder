621c9f166777e531d39fef40282c309c66aa1cb9
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-05-26 14:39:58 -0300

    creating a console app

---
+++ Finder.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Finder.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
