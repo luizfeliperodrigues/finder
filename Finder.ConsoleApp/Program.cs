93b3a93ccf9c04ed536cc257fdbc3249c4794314
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-05-28 11:56:57 -0300

    adicionando referencia e ajustando ConsoleApp

--- Finder.ConsoleApp/Program.cs
+++ Finder.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Finder.Application.Services;
 
 namespace Finder.ConsoleApp
 {
@@ -6,7 +8,37 @@ namespace Finder.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try{
+                Console.Write("Digite a hora que deseja saber? ");
+                string horario = Console.ReadLine();
+
+                if(String.IsNullOrEmpty(horario) || String.IsNullOrWhiteSpace(horario))
+                {
+                    throw new ArgumentNullException();
+                }
+
+                string filePath = @"C:\Users\Felipe\Desktop\study\Aditum\restaurant-hours - Populated.csv";
+
+                FinderService finderService = new FinderService();
+
+                List<string> result = finderService.GetOpenedRestaurants(horario, filePath);
+
+                foreach (var restaurant in result)
+                {
+                    Console.WriteLine(restaurant);
+                }
+
+                // Console.WriteLine(result.Count);
+
+            }
+            catch(ArgumentNullException error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
+            }
+            catch (System.Exception error)
+            {
+                Console.WriteLine($"Error: {error.Message}");
+            }
         }
     }
 }
