d63c45cabc23a41a35012ed4f4c5a7ca092414ca
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-05-29 15:08:48 -0300

    testes do Domain implementados

--- Finder.Domain.Tests/RestaurantServiceTest.cs
+++ Finder.Domain.Tests/RestaurantServiceTest.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
+using FluentAssertions;
 using Finder.Application.Domain.Services;
+using Finder.Application.Domain.Entities;
 
 namespace Finder.Domain.Tests
 {
@@ -13,9 +17,168 @@ namespace Finder.Domain.Tests
         }
 
         [Fact]
-        public void It_Should_Return_False()
+        public void Should_Return_An_Empty_List()
         {
-            
+            List<Restaurant> restaurants = new List<Restaurant>
+            {
+                new Restaurant( 
+                    "Restaurante 1",
+                    new TimeSpan(9,0,0),
+                    new TimeSpan(18,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 2",
+                    new TimeSpan(11,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 3",
+                    new TimeSpan(7,0,0),
+                    new TimeSpan(15,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 4",
+                    new TimeSpan(14,0,0),
+                    new TimeSpan(23,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 5",
+                    new TimeSpan(10,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+            };
+
+            TimeSpan timeTest = new TimeSpan(6,0,0);
+
+            var result = this.restaurantService.GetAvailableRestaurants(timeTest, restaurants);
+
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void Should_Return_The_Correct_List_Size()
+        {
+            List<Restaurant> restaurantsTested = new List<Restaurant>
+            {
+                new Restaurant( 
+                    "Restaurante 1",
+                    new TimeSpan(9,0,0),
+                    new TimeSpan(18,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 2",
+                    new TimeSpan(11,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 3",
+                    new TimeSpan(7,0,0),
+                    new TimeSpan(15,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 4",
+                    new TimeSpan(14,0,0),
+                    new TimeSpan(23,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 5",
+                    new TimeSpan(10,0,0),
+                    new TimeSpan(22,0,0)
+                )
+            };
+
+            List<Restaurant> expected = new List<Restaurant>
+            {
+                new Restaurant( 
+                    "Restaurante 1",
+                    new TimeSpan(9,0,0),
+                    new TimeSpan(18,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 2",
+                    new TimeSpan(11,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 3",
+                    new TimeSpan(7,0,0),
+                    new TimeSpan(15,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 5",
+                    new TimeSpan(10,0,0),
+                    new TimeSpan(22,0,0)
+                )
+            };
+
+            TimeSpan timeTest = new TimeSpan(12,0,0);
+
+            var actual = this.restaurantService.GetAvailableRestaurants(timeTest, restaurantsTested);
+
+            Assert.Equal(expected.Count, actual.Count);
+        }
+
+        [Fact]
+        public void Should_Return_An_Expected_List_Of_Restaurants()
+        {
+            List<Restaurant> restaurantsTested = new List<Restaurant>
+            {
+                new Restaurant( 
+                    "Restaurante 1",
+                    new TimeSpan(9,0,0),
+                    new TimeSpan(18,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 2",
+                    new TimeSpan(11,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 3",
+                    new TimeSpan(7,0,0),
+                    new TimeSpan(15,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 4",
+                    new TimeSpan(14,0,0),
+                    new TimeSpan(23,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 5",
+                    new TimeSpan(10,0,0),
+                    new TimeSpan(22,0,0)
+                )
+            };
+
+            List<Restaurant> expected = new List<Restaurant>
+            {
+                new Restaurant( 
+                    "Restaurante 1",
+                    new TimeSpan(9,0,0),
+                    new TimeSpan(18,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 2",
+                    new TimeSpan(11,0,0),
+                    new TimeSpan(22,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 3",
+                    new TimeSpan(7,0,0),
+                    new TimeSpan(15,0,0)
+                ),
+                new Restaurant( 
+                    "Restaurante 5",
+                    new TimeSpan(10,0,0),
+                    new TimeSpan(22,0,0)
+                )
+            };
+
+            TimeSpan timeTest = new TimeSpan(11,30,0);
+
+            var actual = this.restaurantService.GetAvailableRestaurants(timeTest, restaurantsTested);
+
+            actual.Should().BeEquivalentTo(expected);
         }
     }
 }
--- Finder.Domain.Tests/RestaurantTest.cs
+++ Finder.Domain.Tests/RestaurantTest.cs
@@ -1,7 +1,41 @@
+using System;
+using Finder.Application.Domain.Entities;
+using Xunit;
+
 namespace Finder.Domain.Tests
 {
     public class RestaurantTest
     {
-        
+        [Fact]
+        public void Should_Return_Restaurant_Is_Opened()
+        {
+            var restaurant = new Restaurant(
+                "Restaurante Teste",
+                new TimeSpan(10,0,0),
+                new TimeSpan(22,0,0)
+            );
+
+            var time = new TimeSpan(15,0,0);
+            
+            var result = restaurant.IsOpen(time);
+
+            Assert.True(result, "Restaurant is opened.");
+        }
+
+        [Fact]
+        public void Should_Return_Restaurant_Is_Closed()
+        {
+            var restaurant = new Restaurant(
+                "Restaurante Teste",
+                new TimeSpan(10,0,0),
+                new TimeSpan(22,0,0)
+            );
+
+            var time = new TimeSpan(8,0,0);
+            
+            var result = restaurant.IsOpen(time);
+
+            Assert.False(result, "Restaurant is closed.");
+        }
     }
 }
