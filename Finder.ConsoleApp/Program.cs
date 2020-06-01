6a4afc618c93fc49651bafcb8f9594423577dc1c
Luiz Felipe <feliperodrigues.ime@gmail.com>
2020-06-01 15:00:07 -0300

    ajuste final na validacao de dados em ConsoleApp

--- Finder.ConsoleApp/Program.cs
+++ Finder.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using Finder.Application.Services;
 
@@ -10,20 +11,20 @@ namespace Finder.ConsoleApp
         static void Main(string[] args)
         {
             try{
-                Regex timePattern = new Regex(@"^([0-2][0-3]|[0-1][0-9]):[0-5][0-9]+$") ;
-
                 Console.Write("Digite a hora que deseja saber? ");
                 string time = Console.ReadLine();
 
-                Match match = timePattern.Match(time);
-
-                if(String.IsNullOrEmpty(time) || String.IsNullOrWhiteSpace(time) || !match.Success)
-                {
-                    throw new ArgumentException("Horario inserido invalido.");
-                }
-                
                 Console.Write("Digite o caminho do arquivo CSV? ");
                 string filePath = Console.ReadLine();
+
+                Regex timePattern = new Regex(@"^([0-2][0-3]|[0-1][0-9]):[0-5][0-9]+$");
+                Match match = timePattern.Match(time);
+
+                if(!File.Exists(filePath))
+                    throw new FileNotFoundException("Arquivo não encontrado ou caminho invalido.");
+                
+                if(String.IsNullOrEmpty(time) || String.IsNullOrWhiteSpace(time) || !match.Success)
+                    throw new ArgumentException("Horario inserido invalido.");
 
                 IFinderService finderService = new FinderService();
 
